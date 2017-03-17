5ad2f292a5180d19b5ab9d4facd1580f85a3863c
SatoShun <maro@sato.local>
2017-03-17 13:50:55 +0900

    finished chapter4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,13 +9,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
   	get root_url
   	assert_response :success
-  	assert_select "title", "Home | #{@base_title}"
+  	assert_select "title", "#{@base_title}"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}" #titleタグ内に指定した文字列が存在するか
+    assert_select "title", "#{@base_title}" #titleタグ内に指定した文字列が存在するか
   end
 
   test "should get help" do
